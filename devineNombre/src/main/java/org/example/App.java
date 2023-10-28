cd862985ce0f4fd7db1382f05f75805815b6cc16
Floow82 <128236094+Floow82@users.noreply.github.com>
2023-10-28 15:37:31 +0200

    Ajout de la fonction de devinette (#1)

--- devineNombre/src/main/java/org/example/App.java
+++ devineNombre/src/main/java/org/example/App.java
@@ -1,13 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        devine(12);
+
+    }
+
+
+    public static void devine(int i){
+        int j =0;
+        Scanner sc = new Scanner(System.in);
+        while ( j != i) {
+            System.out.printf("Entrez un nombre : \n");
+            j = sc.nextInt();
+            if (j==i ) {
+                System.out.printf("BRAVOOOO");
+            } else System.out.printf("NUL");
+
+
+        }
     }
 }
